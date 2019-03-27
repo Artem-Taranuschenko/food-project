828627a920f27c9d340bfa5a317a8c20a0017c00
Artem Taranuschenko <taranuschenkoartem@gmail.com>
2019-03-27 21:08:05 +0200

    Added links and smooth scrolling.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
 $( document ).ready(function () {
-
+    /* For the sticky navigation */
     $('.js--section-features').waypoint(function (direction) {
         if (direction == 'down') {
             $('nav').addClass('sticky');
@@ -10,11 +10,12 @@ $( document ).ready(function () {
         offset: '60px'
     });
 
-/*
-    var waypoints = $('#handler-first').waypoint(function(direction) {
-        notify(this.element.id + ' hit 25% from top of window)
-    }, {
-        offset: '25%'
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').on('click', function () {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
     });
-*/
+
+    $('.js--scroll-to-start').on('click', function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
 });
